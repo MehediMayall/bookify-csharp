74b30f4ccb82c8283d23c5225be68a8796b3a0ca
mehedi <mehedisun@outlook.com>
2024-10-07 14:26:53 +0600

    Added IDomainEvent interface

--- src/Bookify.Domain/Abstractions/Entity.cs
+++ src/Bookify.Domain/Abstractions/Entity.cs
@@ -3,9 +3,19 @@ namespace Bookify.Domain;
 
 public abstract class Entity {
     public Guid Id {get; init;}
+    private readonly List<IDomainEvent> domainEvents = new();
 
     public Entity(Guid id)
     {
         Id = id;
     }
+
+    public IReadOnlyCollection<IDomainEvent> GetDomainEvents() =>
+        domainEvents.ToList();
+
+    public void ClearDomainEvents() =>
+        domainEvents.Clear();
+
+    public void RaiseDomainEvent(IDomainEvent domainEvent) =>
+        domainEvents.Add(domainEvent);
 }
---
+++ src/Bookify.Domain/Abstractions/IDomainEvents.cs
@@ -0,0 +1,7 @@
+using MediatR;
+
+namespace Bookify.Domain;
+
+public interface IDomainEvent: INotification{
+    
+}
